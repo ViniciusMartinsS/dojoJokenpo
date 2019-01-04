303de2bbfeb97eec8b9462aa8298589496708cc4
ViniciusMartinsS <viniciussimonemartins@yahoo.com.br>
2019-01-04 10:53:20 -0200

    Melhor TDD | Automatizando sem interação de usuário | Version 4.0

--- test.js
+++ test.js
@@ -1,11 +1,75 @@
 const { deepEqual } = require("assert");
 
-const { jokenpo } = require("./index");
+const judge = require("./Services/Judge");
 
 describe("Execução Função Jokenpo", () => {
-  it("Verificando a decisão do juiz de vitória em Player01 X Player02", async () => {
+  it("Verificando a decisão do juiz de vitória para computador ou jogador02: Papel X Tesoura", async () => {
+    const expected = "Vitória: |Computador - Tesoura|";
+    const resultado = judge.getWinner("Papel", "Tesoura", "Computador");
+    deepEqual(resultado, expected);
+  }).timeout(Infinity);
+});
+
+describe("Execução Função Jokenpo", () => {
+  it("Verificando a decisão do juiz de vitória para computador ou jogador02: Tesoura X Pedra", async () => {
+    const expected = "Vitória: |Computador - Pedra|";
+    const resultado = judge.getWinner("Tesoura", "Pedra", "Computador");
+    deepEqual(resultado, expected);
+  }).timeout(Infinity);
+});
+
+describe("Execução Função Jokenpo", () => {
+  it("Verificando a decisão do juiz de vitória para computador ou jogador02: Pedra X Papel", async () => {
+    const expected = "Vitória: |Computador - Papel|";
+    const resultado = judge.getWinner("Pedra", "Papel", "Computador");
+    deepEqual(resultado, expected);
+  }).timeout(Infinity);
+});
+//
+describe("Execução Função Jokenpo", () => {
+  it("Verificando a decisão do juiz de vitória para jogador01: Tesoura X Papel", async () => {
+    const expected = "Vitória: |Jogador01 - Tesoura|";
+    const resultado = judge.getWinner("Tesoura", "Papel", "Computador");
+    deepEqual(resultado, expected);
+  }).timeout(Infinity);
+});
+
+describe("Execução Função Jokenpo", () => {
+  it("Verificando a decisão do juiz de vitória para jogador01: Pedra X Tesoura", async () => {
+    const expected = "Vitória: |Jogador01 - Pedra|";
+    const resultado = judge.getWinner("Pedra", "Tesoura", "Computador");
+    deepEqual(resultado, expected);
+  }).timeout(Infinity);
+});
+
+describe("Execução Função Jokenpo", () => {
+  it("Verificando a decisão do juiz de vitória para jogador01: Papel X Pedra", async () => {
     const expected = "Vitória: |Jogador01 - Papel|";
-    const resultado = jokenpo();
+    const resultado = judge.getWinner("Papel", "Pedra", "Computador");
+    deepEqual(resultado, expected);
+  }).timeout(Infinity);
+});
+
+describe("Execução Função Jokenpo", () => {
+  it("Verificando a decisão do juiz de empate para: Tesoura X Tesoura", async () => {
+    const expected = "Empate! ";
+    const resultado = judge.getWinner("Tesoura", "Tesoura", "Computador");
+    deepEqual(resultado, expected);
+  }).timeout(Infinity);
+});
+
+describe("Execução Função Jokenpo", () => {
+  it("Verificando a decisão do juiz de empate para: Pedra X Pedra", async () => {
+    const expected = "Empate! ";
+    const resultado = judge.getWinner("Pedra", "Pedra", "Computador");
+    deepEqual(resultado, expected);
+  }).timeout(Infinity);
+});
+
+describe("Execução Função Jokenpo", () => {
+  it("Verificando a decisão do juiz de empate para: Papel X Papel", async () => {
+    const expected = "Empate! ";
+    const resultado = judge.getWinner("Papel", "Papel", "Computador");
     deepEqual(resultado, expected);
   }).timeout(Infinity);
 });
